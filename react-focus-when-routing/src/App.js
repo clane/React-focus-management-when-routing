79611fa59e645b8c358a1cdeebbac53005d19901
Chris Lane <chris.lane359@gmail.com>
2018-11-29 22:13:36 -0800

    early progress

--- react-focus-when-routing/src/App.js
+++ react-focus-when-routing/src/App.js
@@ -2,23 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Helmet } from "react-helmet";
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+	Link
+} from 'react-router-dom'
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+			    <h1>Focus management when routing</h1>
         </header>
       </div>
     );
